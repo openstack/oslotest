0b3c98a34900b49d5efd95b80b417df165536eb1
Stephen Finucane <stephenfin@redhat.com>
2019-12-19 09:40:40 +0000

    trivial: Cleanup of doc config file
    Change-Id: Ie5f147cff6e92ce64c32b50a01e65c042dbc0b74
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -12,34 +13,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath('../..'))
-# -- General configuration ----------------------------------------------------
+# -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
-    #'sphinx.ext.intersphinx',
     'openstackdocstheme',
 ]
 
-# autodoc generation is a bit aggressive and a nuisance when doing heavy
-# text edit cycles.
-# execute "export SPHINX_DEBUG=1" in your terminal to disable
 modindex_common_prefix = ['oslotest.']
-
-# The suffix of source filenames.
-source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
 
 # General information about the project.
 project = u'oslotest'
-copyright = u'2014, OpenStack Foundation'
+copyright = u'2014-2019, OpenStack Foundation'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
@@ -51,37 +41,20 @@ add_module_names = True
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-# -- Options for HTML output --------------------------------------------------
+
+# -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-# html_theme_path = ["."]
-# html_static_path = ['static']
 html_theme = 'openstackdocs'
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-# html_last_updated_fmt = '%b %d, %Y'
 html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
-# Output file base name for HTML help builder.
-htmlhelp_basename = '%sdoc' % project
 
-# openstackdocstheme options
+# -- openstackdocstheme configuration ----------------------------------------
+
 repository_name = 'openstack/oslotest'
 bug_project = 'oslotest'
 bug_tag = ''
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title, author, documentclass
-# [howto/manual]).
-latex_documents = [
-    ('index',
-     '%s.tex' % project,
-     u'%s Documentation' % project,
-     u'OpenStack Foundation', 'manual'),
-]
-
-# Example configuration for intersphinx: refer to the Python standard library.
-#intersphinx_mapping = {'http://docs.python.org/': None}
-
